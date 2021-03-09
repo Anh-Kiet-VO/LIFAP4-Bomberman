7d3e3524217a76ade4fef9b33a89ade787a87a40
Cécilia <cecilia.nguyen@etu.univ-lyon1.fr>
2021-03-09 09:22:47 +0100

    Modif de Bombe.h

--- src/Bombe.h
+++ src/Bombe.h
@@ -2,7 +2,19 @@
 #define BOMBE_H
 
 class Bombe {
+    private:
+    unsigned int posX, posY;
+    unsigned int portee;
 
+    public:
+    Bombe();
+    Bombe(unsigned int positionX, unsigned int positionY, unsigned int porteeBombe);
+    ~Bombe();
+
+    unsigned int getPosX() const;
+    unsigned int getPosY() const;
+    unsigned int getPortee() const;
+    void setPortee(unsigned int p);
 };
 
 #endif
