a916ba0a92e302d105110b64b637dafb045b2cdd
Kevin69100 <keazcu@gmail.com>
2021-03-09 10:24:57 +0100

    modification module Brique

--- src/Brique.cpp
+++ src/Brique.cpp
@@ -1,7 +1,23 @@
 #include "Brique.h"
 #include <cassert>
-#include <fstream>
 #include <stdlib.h>
 
 #include <iostream>
 using namespace std;
+
+Brique::Brique(){
+    posX = posY = 0;
+    bonus = false;
+    typeBonus = "NULL";
+    coulBrique = Couleur(0,0,0);
+}
+
+Brique::Brique(int x, int y, bool det, string typeBon, Couleur c){
+    assert(x >= 0 && y >= 0);
+    posX = x;
+    posY = y;
+    détruit = det;
+    typeBonus = typeBon;
+    coulBrique = c;
+}
+
--- src/Brique.h
+++ src/Brique.h
@@ -1,25 +1,78 @@
-#ifndef IMAGE_H
-#define IMAGE_H
+/**
+  @brief Module gérant une Brique
+  @file Brique.h
+
+  @date 9 mars 2021
+*/      
+
+#ifndef BRIQUE_H
+#define BRIQUE_H
 
 #include "Couleur.h"
 
 #include <string>
 class Brique{
     private:
+        /**
+         * @brief
+         * Le booléen pour indiquer si la brique est détruite
+        */
         bool détruit;
+
+        /**
+         * @brief
+         * Le booléen pour indiquer si la brique contient un bonus
+        */
         bool bonus;
+
+        /**
+         * @brief
+         * Indique le type de bonus de la brique
+        */
         string typeBonus;
-        Couleur coolBrique;
+
+        /**
+         * @brief
+         * Couleur de la brique
+        */
+        Couleur coulBrique;
 
     public:
+        /**
+         * @brief
+         * Position de la brique
+        */
         int posX, posY;
 
+        /**
+         * @brief
+         * Constructeur par défaut de la classe: initialise sa position, si il a un bonus,
+         * son type de bonus et sa couleur.
+         @param posX : 
+        */
         Brique();
 
-        Brique(int x, int y, bool det, bool typeBon, Couleur coul);
+        /**
+         * @brief
+         * Constructeur de la classe: initialise sa position, si il a un bonus,
+         * son type de bonus et sa couleur.
+         @param x : entier
+         @param y : entier
+         @param det : booléen
+         @param typeBon : chaine de caractères
+         @param coul : Couleur
+        */
+        Brique(int x, int y, bool det, string typeBon, Couleur c);
 
+        /**
+         * @brief
+         * Destructeur de la classe: mise à jour de posX et posY à 0, détruit à false,
+         * typeBonus à null, et couleur à noire. 
+        */
         ~Brique();
 
         int getPosX();
         int getPosY();
-}
+};
+
+#endif
