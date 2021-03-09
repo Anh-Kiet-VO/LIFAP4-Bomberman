10672a2913ea442f5cd4927108e95010c2eb9e9a
Cécilia <cecilia.nguyen@etu.univ-lyon1.fr>
2021-03-09 11:18:38 +0100

    Ajout Bombe.cpp, Bombe.h et Makefile

--- src/Bombe.cpp
+++ src/Bombe.cpp
@@ -1,1 +1,18 @@
 #include "Bombe.h"
+
+Bombe::Bombe() {
+    posX = posY = 0;
+    portee = 0;
+}
+
+Bombe::Bombe(unsigned int positionX, unsigned int positionY, unsigned int nouvPortee) {
+    posX = positionX;
+    posY = positionY;
+    portee = nouvPortee;
+}
+
+Bombe::~Bombe() {
+    posX = posY = 0;
+    portee = 0;
+}
+
