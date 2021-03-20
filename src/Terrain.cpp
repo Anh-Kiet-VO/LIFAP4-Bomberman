9db5ec89717006de082d04b150c45c36a5b191d3
Anh-Kiet <anhkietakpro@gmail.com>
2021-03-20 15:41:50 +0100

    Terrain

---
+++ src/Terrain.cpp
@@ -0,0 +1,49 @@
+#include "Terrain.h"
+#include <cassert>
+
+const char terrain1[20][27] = {
+"##########################",
+"#..$$$$$#$$$$$#$$$$$#$$..#",
+"#.$$$$$$$$$$$$$$$$$$$$$$.#",
+"#$$$$$$$#$$$$$#$$$$$#$$$$#",
+"#$$$$$$$$$$$$$$$$$$$$$$$$#",
+"#$$$$$$$#$$$$$#$$$$$#$$$$#",
+"#$$$$$$$$$$$$$$$$$$$$$$$$#",
+"#$$$$$$$#$$$$$#$$$$$#$$$$#",
+"#$$$$$$$$$$$$$$$$$$$$$$$$#",
+"#.$$$$$$#$$$$$#$$$$$#$$$.#",
+"#..$$$$$$$$$$$$$$$$$$$$..#",
+"##########################"
+};
+
+Terrain::Terrain() {
+	dimX = 27;
+	dimY = 20;
+	for(int x = 0 ; x < dimX ; x++) {
+		for(int y = 0 ; y < dimY ; y++) {
+			ter[x][y] = terrain1[dimY-1-y][x];
+		}
+	}
+}
+
+bool Terrain::isValidPos(const unsigned int x, const unsigned int y) const {
+	return ( (x >= 0) && (x < dimX) && (y >= 0) && (y < dimY) && (ter[x][y] !='#') );
+}
+
+char Terrain::getXY(const unsigned int x, const unsigned int y) const {
+	assert(x >= 0);
+	assert(y >= 0);
+	assert(x < dimX);
+	assert(y < dimY);
+
+	return ter[x][y];
+}
+
+int Terrain::getDimX () const {
+	return dimX;
+}
+
+int Terrain::getDimY () const {
+	return dimY;
+}
+
---
+++ src/Terrain.h
@@ -0,0 +1,52 @@
+/**
+ * @brief Module gérant le terrain du Bomberman
+ * Le terrain dst un tableau 2D de caractère dont la taille est de dimX * dimY
+ * @file Terrain.h
+ * @date 09/03/2021
+*/
+
+#ifndef _TERRAIN_H
+#define _TERRAIN_H
+
+/**
+ * @brief La classe Terrain contient ses dimensions et un tab 2D de cases (une case= 1 char)
+*/
+
+class Terrain {
+	private:
+	int dimX;
+	int dimY;
+	char ter[100][100];
+
+	public:
+	/**
+	 * @brief Le constructeur remplit dimX et dimY et les cases du tableau ter avec un terrain par défaut
+	*/
+	Terrain();
+
+	/**
+	 * @brief Booléen qui renvoie vrai si on peut positionner le personnage en X,Y
+	 * @param x : entier
+	 * @param y : entier
+	*/
+	bool isValidPos(const unsigned int x, const unsigned int y) const;
+
+    /**
+	 * @brief Renvoie le type d'objet se trouvant en X,Y
+	 * @param x : entier
+	 * @param y : entier
+    */
+    char getXY(const unsigned int x, const unsigned int y) const;
+
+    /**
+	 * @brief Renvoie la largeur du terrain en X
+    */
+    int getDimX() const;
+
+    /**
+	 * @brief Renvoie la hauteur du terrain Y
+    */
+    int getDimY() const;
+};
+
+#endif
