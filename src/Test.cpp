35004ea77704e836921d882a1f24d3fb0519afb8
Kevin69100 <keazcu@gmail.com>
2021-03-23 09:00:43 +0100

    Test regression

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,9 +1,12 @@
 #include "TabBombe.h"
+#include "TabBrique.h"
 #include <iostream>
 using namespace std;
 
 int main() {
     TabBombe bombes;
     bombes.testRegression();
+    TabBrique briques;
+    briques.testRegression();
     return 0;
 }
