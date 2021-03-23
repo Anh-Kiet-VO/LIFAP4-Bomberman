6b2fe755f915d65d442059b22594ded31e9e88b4
Kevin69100 <keazcu@gmail.com>
2021-03-23 09:31:12 +0100

    testregression

--- src/Test.cpp
+++ src/Test.cpp
@@ -9,10 +9,7 @@ int main() {
     bombes.testRegression();
     TabBrique briques;
     briques.testRegression();
-<<<<<<< HEAD
-=======
     Personnage perso;
     perso.testRegression();
->>>>>>> 84333de8f920ee4190f7dce652abfc22ce249b28
     return 0;
 }
