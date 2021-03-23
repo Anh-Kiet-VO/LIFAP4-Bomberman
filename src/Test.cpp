84333de8f920ee4190f7dce652abfc22ce249b28
Anh-Kiet <anhkietakpro@gmail.com>
2021-03-23 09:10:42 +0100

    personnage testRegression

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,19 +1,15 @@
 #include "TabBombe.h"
 #include "TabBrique.h"
+#include "Personnage.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-    TabBombe tab_bo;
-    tab_bo.testRegression();
-    TabBrique tab_br;
-    tab_br.testRegression();
-=======
     TabBombe bombes;
     bombes.testRegression();
     TabBrique briques;
     briques.testRegression();
->>>>>>> 35004ea77704e836921d882a1f24d3fb0519afb8
+    Personnage perso;
+    perso.testRegression();
     return 0;
 }
