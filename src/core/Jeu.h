ee537ff1d4b64cb83458430a683c332bffffaffb
p1909458 <kevin.cuzin@etu.univ-lyon1.fr>
2021-04-06 12:22:54 +0200

    création 2ème perso

--- src/core/Jeu.h
+++ src/core/Jeu.h
@@ -4,7 +4,6 @@
  * @date 20/03/2021
 */
 
-
 #ifndef _JEU_H
 #define _JEU_H
 
@@ -14,19 +13,26 @@
 class Jeu {
 	private:
 	Terrain ter;
-	Personnage perso;
+    Personnage perso;
+    Personnage in_perso;
     Bombe b;
 	
 	public:
     Jeu();
 
     Terrain& getTerrain();
-    Personnage& getPerso();
+    Personnage& getPerso(int i);
     Bombe& getBombe();
 
     const Terrain& getConstTerrain() const;
-    const Personnage& getConstPerso() const;
+    const Personnage& getConstPerso(int i) const;
     const Bombe& getConstBombe() const;
+
+    void ExploserBombe(Personnage& po, Terrain& ter, Bombe& b);
+
+    void PlacerBombe(Personnage& po, Terrain& ter, Bombe& b);
+
+    void PlaceEtExplose(Personnage& po, Terrain& ter, Bombe& b);
 
     bool actionClavier(const char touche);
 };
