0e7f88ca51319b615418bb1eb36c597e82f3e35b
Anh-Kiet <anhkietakpro@gmail.com>
2021-04-13 09:26:11 +0200

    Indentation

--- src/core/Jeu.h
+++ src/core/Jeu.h
@@ -21,52 +21,51 @@ class Jeu {
     Bombe b2;
 	
 	public:
+        /**
+         * @brief Mutateur : modifie la position de la bombe
+        */
+        Jeu();
 
-    /**
-     * @brief Mutateur : modifie la position de la bombe
-    */
-    Jeu();
+        Terrain& getTerrain();
+        Personnage& getPerso(int i);
+        Bombe& getBombe(int j);
 
-    Terrain& getTerrain();
-    Personnage& getPerso(int i);
-    Bombe& getBombe(int j);
+        /**
+         * @brief accesseur : permet d'accéder au terrain
+        */
+        const Terrain& getConstTerrain() const;
 
-    /**
-     * @brief accesseur : permet d'accéder au terrain
-    */
-    const Terrain& getConstTerrain() const;
+        /**
+         * @brief accesseur : permet d'accéder à un personnage
+         * @param i : entier
+        */
+        const Personnage& getConstPerso(int i) const;
 
-    /**
-     * @brief accesseur : permet d'accéder à un personnage
-     * @param i : entier
-    */
-    const Personnage& getConstPerso(int i) const;
+        /**
+         * @brief accesseur : permet d'accéder à une bombe
+         * @param j : entier
+        */
+        const Bombe& getConstBombe(int j) const;
 
-    /**
-     * @brief accesseur : permet d'accéder à une bombe
-     * @param j : entier
-    */
-    const Bombe& getConstBombe(int j) const;
+        
+        /*void ExploserBombe(Personnage& po, Terrain& ter, Bombe& b);
 
-    
-    /*void ExploserBombe(Personnage& po, Terrain& ter, Bombe& b);
+        void PlacerBombe(Personnage& po, Terrain& ter, Bombe& b);*/
 
-    void PlacerBombe(Personnage& po, Terrain& ter, Bombe& b);*/
+        /**
+         * @brief Permet de placer et de faire exploser les briques autour d'une bombe
+         * @param j : entier
+         * @param po : personnage
+         * @param ter : terrain
+         * @param b : bombe
+        */
+        void PlaceEtExplose(Personnage& po, Terrain& ter, Bombe& b);
 
-    /**
-     * @brief Permet de placer et de faire exploser les briques autour d'une bombe
-     * @param j : entier
-     * @param po : personnage
-     * @param ter : terrain
-     * @param b : bombe
-    */
-    void PlaceEtExplose(Personnage& po, Terrain& ter, Bombe& b);
-
-    /**
-     * @brief Permet de réaliser une action en appuyant sur une touche du clavier
-     * @param touche : caractère
-    */
-    bool actionClavier(const char touche);
+        /**
+         * @brief Permet de réaliser une action en appuyant sur une touche du clavier
+         * @param touche : caractère
+        */
+        bool actionClavier(const char touche);
 };
 
 #endif
